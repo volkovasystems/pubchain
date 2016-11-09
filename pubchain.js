08bebb20871c7bd571b47e2cc48c9dc6085454ca
volkovasystems <richeve.bebedor@gmail.com>
2016-11-10 06:11:39 +0800

    Finished pubchain procedure.

---
+++ pubchain.js
@@ -0,0 +1,77 @@
+"use strict";
+
+/*;
+	@module-license:
+		The MIT License (MIT)
+		@mit-license
+
+		Copyright (@c) 2016 Richeve Siodina Bebedor
+		@email: richeve.bebedor@gmail.com
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"package": "pubchain",
+			"path": "pubchain/pubchain.js",
+			"file": "pubchain.js",
+			"module": "pubchain",
+			"author": "Richeve S. Bebedor",
+			"eMail": "richeve.bebedor@gmail.com",
+			"repository": "https://github.com/volkovasystems/pubchain.git",
+			"test": "pubchain-test.js",
+			"global": true
+		}
+	@end-module-configuration
+
+	@module-documentation:
+		Smart deep list of object enumerable properties.
+	@end-module-documentation
+
+	@include:
+		{
+			"propchain": "propchain"
+		}
+	@end-include
+*/
+
+if( typeof require == "function" ){
+	var propchain = require( "propchain" );
+}
+
+if( typeof window != "undefined" && !( "propchain" in window ) ){
+	throw new Error( "propchain is not defined" );
+}
+
+var pubchain = function pubchain( entity ){
+	/*;
+		@meta-configuration:
+			{
+				"entity:required": "*"
+			}
+		@end-meta-configuration
+	*/
+
+	return propchain( entity ).filter( ( definition ) => { return definition.enumerable; } );
+};
+
+if( typeof module != "undefined" && typeof module.exports != "undefined" ){
+	module.exports = pubchain;
+}
